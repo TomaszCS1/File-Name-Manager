edbee216776d0716015cb65408995b8b22d85355
Thomas Pfaffinger <thomas.pfaffinger@softronik.de>
2022-11-11 15:09:50 +0100

    read path from folder

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -15,19 +15,14 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Collections;
-//using System.Windows.Forms;
-
-
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
 
 namespace WpfApp2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
 
-        string filePath;
         string directPath;
         string[] files;
         public string anzBuchstabenStr;
@@ -35,39 +30,32 @@ namespace WpfApp2
 
         public MainWindow()
         {
+
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            
-            if (openFileDialog.ShowDialog() == true)
-            {
-                //Get the path of specified file
-                filePath = openFileDialog.FileName;
-                tb1.Text = filePath;
-
-            }
-
-        }
+        
 
 
         private void btnClose(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
         }
 
         private void btnOpenDiretory(object sender, RoutedEventArgs e)
         {
-
-
-            OpenFileDialog openFileDialogDir = new OpenFileDialog();
-            if (openFileDialogDir.ShowDialog() == true)
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                filePath = openFileDialogDir.FileName;
-                directPath = System.IO.Path.GetDirectoryName(filePath);
+                directPath = fbd.SelectedPath;
+            
+            }
+
+            //OpenFileDialog openFileDialogDir = new OpenFileDialog();                                                 <==  read folder path from file  
+            //if (openFileDialogDir.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //{
+            //    filePath = openFileDialogDir.FileName;
+            //    directPath = System.IO.Path.GetDirectoryName(filePath);
 
 
 
@@ -78,25 +66,14 @@ namespace WpfApp2
                 for (int i = 0; i < files.Length; i++)
                 {
                     lbFilesInDirectory.Items.Add(i + 1 + ". " + System.IO.Path.GetFileName(files[i]));
-
                 }
-            }
-        }
-
-
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            //FolderBrowserDialog fbd = new FolderBrowserDialog;
-
-
-        }
+         }
+        
 
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // string copySelected = lbFilesInDirectory.SelectedItem.ToString();
-            //tbName.Text = copySelected;
+          
         }
 
         private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
@@ -106,17 +83,12 @@ namespace WpfApp2
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //lbFilesNewNames.Items.Add(tbName.Text);
-            //int c = lbFilesNewNames.Items.Count;
-            //Console.WriteLine("NAME TEMP= "+c);     
-
-
-
+           
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //test
+           
         }
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
@@ -230,7 +202,7 @@ namespace WpfApp2
             }
             else if (files == null)
             {
-                MessageBox.Show("Choose directory -> DIRECTORY OPEN");
+                System.Windows.MessageBox.Show("Choose directory -> DIRECTORY OPEN");
 
             }
         }
